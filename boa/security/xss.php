b95c4759820788a7186dd1361aed9f10bc6fa05b
B.S <poetbi@gmail.com>
2022-06-18 16:42:36 +0800

    正则优化

--- boa/security/xss.php
+++ boa/security/xss.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Author  : poetbi (poetbi@163.com)
-Document: http://boasoft.top/doc/#api/boa.security.xss.html
+Document: http://boasoft.top/doc/api/boa.security.xss.html
 Licenses: Apache-2.0 (http://apache.org/licenses/LICENSE-2.0)
 */
 namespace boa\security;
@@ -11,17 +11,17 @@ use boa\base;
 class xss extends base{
 	protected $cfg = [
 		'tags' => ['script', 'base'],
-		'events' => ['onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload']
+		'events' => ['on*']
 	];
 
 	function filter($str){
 		$tag = implode('|', $this->cfg['tags']);
-		$str = preg_replace("/<($tag)[^>]*>(.*?)<\/($tag)>/i", '', $str);
+		$str = preg_replace("/<($tag)[^>]*>([\s\S]*?)<\/($tag)>/i", '', $str);
 		$str = preg_replace("/<($tag)[^>]*>/i", '', $str);
 
 		$event = implode('|', $this->cfg['events']);
-		$str = preg_replace("/ ($event)='(.+?)'( |>)/i", '$3', $str);
-		$str = preg_replace("/ ($event)=\"(.+?)\"( |>)/i", '$3', $str);
+		$event = str_replace('*', '[a-z]+', $event);
+		$str = preg_replace("/<([^>]+?)\s+($event)\s*=\s*(\"|')([\s\S]+?)(\"|')(\s+|>)/i", '<$1 $6', $str);
 
 		return $str;
 	}
